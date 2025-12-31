7b919df0eab235796fdb31f79c9d00ec7d617248
Vordgi <sasha2822222@gmail.com>
2025-12-31 15:32:58 +0000

    rd-427 add default global styles

--- packages/robindoc/rollup.config.js
+++ packages/robindoc/rollup.config.js
@@ -4,6 +4,7 @@ const typescript = require("@rollup/plugin-typescript");
 const terser = require("@rollup/plugin-terser");
 const sass = require("rollup-plugin-sass");
 const { default: preserveDirectives } = require("rollup-preserve-directives");
+const copy = require("rollup-plugin-copy");
 
 module.exports = {
     input: ["src/index.tsx"],
@@ -42,5 +43,8 @@ module.exports = {
         }),
         terser(),
         preserveDirectives(),
+        copy({
+            targets: [{ src: "src/assets/global.css", dest: "lib" }],
+        }),
     ],
 };
