d34373eaad38bd745eb670f556aee449cf42986f
Vordgi <sasha2822222@gmail.com>
2026-01-23 16:13:12 +0000

    rd-410 base elements improvements

--- packages/robindoc/src/components/ui/table/td/index.tsx
+++ packages/robindoc/src/components/ui/table/td/index.tsx
@@ -5,8 +5,15 @@ import "./td.scss";
 
 interface TdProps {
     className?: string;
+    align?: "left" | "center" | "right" | null;
 }
 
-export const Td: React.FC<React.PropsWithChildren<TdProps>> = ({ className, children }) => {
-    return <td className={clsx("r-td", className)}>{children}</td>;
+const ALIGN_CLASSES = {
+    left: "r-td_left",
+    center: "r-td_center",
+    right: "r-td_right",
+};
+
+export const Td: React.FC<React.PropsWithChildren<TdProps>> = ({ className, children, align }) => {
+    return <td className={clsx("r-td", className, align && ALIGN_CLASSES[align])}>{children}</td>;
 };
